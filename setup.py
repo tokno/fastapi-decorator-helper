9dfac35f162cf46878bdc0930cd2f3dca3cc2bfd
Takuya Okuno <iwashi1629@gmail.com>
2022-12-27 03:29:55 +0900

    commit

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='fastapi-decorator-helper',
     version='0.0.1',
-    author='Takuya Okuno'
+    author='Takuya Okuno',
+    packages=find_packages(),
 )
